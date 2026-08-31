2a766b98e628e53ef065e1ddbe8c96d43d855771
knodelt <thomasknodel@hotmail.de>
2026-08-31 15:05:46 +0200

    fix: polish mobile notifications and version label

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 export const GAME_VERSION = 1;
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.1.0';
 
 export const ECONOMY = Object.freeze({
   // Flüssiges Startkapital; die Startfahrzeuge sind bereits bezahlt.
